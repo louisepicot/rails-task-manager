81067eb4468c8abb54b9eac402f648e6ff063ac3
louisepicot <louisepicot@gmail.com>
2017-10-31 16:26:46 +0100

    jj

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  resources :tasks
+
+  # get 'index', to: "tasks#index"
+
+  # get 'show', to: "tasks#show"
+
+  # get 'new', to: "tasks#new"
+
+  # post 'tasks/:id', to: "tasks#create"
+
+  # get 'tasks/:id/edit', to: "tasks#edit"
+
+  # patch 'tasks/:id', to: "tasks#update"
+
+  # get 'tasks/:id', to: "tasks#destroy"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,16 @@
+require 'faker'
+
+status = ["to do", "in progress" "done"]
+puts 'Creating 10 ...'
+10.times do
+  Task.create!(
+    name:    Faker::Name.name,
+    description: Faker::Name.title,
+    status:  status.sample
+  )
+
+end
+puts 'Finished!'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
